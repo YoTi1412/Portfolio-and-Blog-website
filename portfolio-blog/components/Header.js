66dba8f23517e242d614e16a2b4ac83824eb9ad8
YoTi <yasserrafai5@gmail.com>
2024-05-26 13:25:40 +0100

    Styling the blog pages

--- portfolio-blog/components/Header.js
+++ portfolio-blog/components/Header.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
-export default function Header() {
+export default function Header ()
+{
     return (
         <div className="w-full py-10 px-4 sm:px-6 lg:px-0">
             <div className="flex flex-col sm:flex-row justify-between max-w-3xl mx-auto items-center">
-                <div className="text-3xl sm:text-2xl font-semibold">Awesome Portfolio</div>
+                <div className="text-3xl sm:text-2xl font-semibold">Portfolio & Blog App</div>
                 <ul className="flex mt-4 sm:mt-0">
                     <li>
                         <Link href="/" passHref>
--- portfolio-blog/pages/blog/[slug].js
+++ portfolio-blog/pages/blog/[slug].js
@@ -43,29 +43,28 @@ export default function Portfolio ({ post, content })
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div>
-                <h1>{post.title}</h1>
-                <p>{new Date(post.date).toDateString()}</p>
-                <p>{post.description}</p>
-                <div>
-                    <p>
-                        {post.author.name}
-                        {post.author.image && (
-                            <Image
-                                src={post.author.image.url}
-                                width={post.author.image.width / 3}
-                                height={post.author.image.height / 3}
-                                alt={post.author.name}
-                            />
-                        )}
-                    </p>
-                </div>
-                <div>
+            <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-0">
+                <h1 className="text-6xl text-gray-900 font-bold selection:text-green-700">{post.title}</h1>
+                <div className="flex space-x-3 mt-2">
                     {post.tags && post.tags.map((tag) => (
-                        <span key={tag}>{tag}</span>
+                        <span className="uppercase text-sm tracking-wide m-2 bg-green-200 px-2 py-1 rounded-lg text-green-700" key={tag}>{tag}</span>
                     ))}
                 </div>
-                <div>
+                <div className="flex justify-between items-center mb-8">
+                    <p className="text-gray-700 selection:text-green-700">{new Date(post.date).toDateString()}</p>
+                    <div className="flex items-center">
+                        <p className="mr-4 text-gray-800 text-lg font-semibold selection:text-green-700">{post.author.name}</p>
+                        <Image
+                            className="rounded-full"
+                            src={post.author.image.url}
+                            width={75}
+                            objectFit="cover"
+                            height={75}
+                            alt={post.author.name}
+                        />
+                    </div>
+                </div>
+                <div className="prose prose-xl max-w-none selection:bg-green-300 selection:text-green-900">
                     <MDXRemote {...content} />
                 </div>
             </div>
